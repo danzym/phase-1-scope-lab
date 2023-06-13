ac286bbed2b94b139f1a0d926ccc49a3e7de1116
Danylo Zymbalevskyi <zymbalevskyy@gmail.com>
2023-06-13 12:23:56 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+// Declare customerName in the global scope
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(newFavorite) {
+  bestCustomer = newFavorite;
+}
+
+const leastFavoriteCustomer = "initial value";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "new value";
+}
+
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  bestCustomer,
+  setBestCustomer,
+  overwriteBestCustomer,
+  leastFavoriteCustomer,
+  changeLeastFavoriteCustomer,
+};
